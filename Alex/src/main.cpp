13541ecfe7dc601b40f15217a96d136a519464dc
Pedro Juan Ibarra <Pedro Juan Ibarra@PedroJuanIbarra>
2018-02-03 20:25:38 +0100

    Fase 1 añadida funcionalidad basica del menu

--- Alex/src/main.cpp
+++ Alex/src/main.cpp
@@ -9,7 +9,46 @@
 using namespace std;
 
 int main(){
-	cout << "Hola Mundo";
+	char ch;
+	system ("CLS");
+
+	do {
+		cout << "\t1) Opcion 1"<<endl;
+		cout << "\t2) Opcion 2"<<endl;
+		cout << "\t3) Opcion 3"<<endl;
+		cout << "\t4) Opcion 4"<<endl;
+		cout << "Eliga una o pulse S para salir"<<endl;
+		cin >> ch;
+		switch (ch){
+		case '1':
+			cout<<"Opcion 1"<<endl;
+			cout<< "Pulse cualquier tecla para continuar."<<endl;
+			system("pause");
+			break;
+		case '2':
+			cout<<"Opcion 2"<<endl;
+			cout<< "Pulse cualquier tecla para continuar."<<endl;
+			system("pause");
+			break;
+		case '3':
+			cout<<"Opcion 3"<<endl;
+			cout<< "Pulse cualquier tecla para continuar."<<endl;
+			system("pause");
+			break;
+		case '4':
+			cout<<"Opcion 4"<<endl;
+			cout<< "Pulse cualquier tecla para continuar."<<endl;
+			system("pause");
+			break;
+		default:
+			cout<<"Eliga una opcion valida"<<endl;
+			cout<< "Pulse cualquier tecla para continuar."<<endl;
+			system("pause");
+			system("CLS");
+		}
+
+	}while ((ch!='S') && (ch!='s'));
+
 	return 0;
 }
 
