f6e6c487a2d119f1cc6d85da3601ea51437d8c07
Pedro Juan Ibarra <Pedro Juan Ibarra@PedroJuanIbarra>
2018-02-03 19:43:50 +0100

    Fase 0 añadida funcion main.

---
+++ Alex/src/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 3 feb. 2018
+ *      Author: Pedro Juan Ibarra
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "Hola Mundo";
+	return 0;
+}
+
+
