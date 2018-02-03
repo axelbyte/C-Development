d1da1f1ef8c34a5fee3876d81a7b208a3622c548
Pedro Juan Ibarra <Pedro Juan Ibarra@PedroJuanIbarra>
2018-02-03 21:41:10 +0100

    MIT_Course Practica con clases

--- Alex/src/main.cpp
+++ Alex/src/main.cpp
@@ -6,45 +6,66 @@
  */
 
 #include <iostream>
+#include "CPoint.h"
+
 using namespace std;
+
+void menu() {
+	cout << "\t1) Opcion 1" << endl;
+	cout << "\t2) Opcion 2" << endl;
+	cout << "\t3) Opcion 3" << endl;
+	cout << "\t4) Opcion 4" << endl;
+	cout << "Eliga una o pulse S para salir" << endl;
+}
+
+void footer() {
+	cout << "Pulse cualquier tecla para continuar." << endl;
+	system("pause");
+}
+
+int CPoint::n = 0;
+
+void opcion1(){
+	CPoint a(3,4);
+	CPoint b[5];
+	CPoint *c = new CPoint;
+
+	cout << a.n << endl; // imprime 7
+	delete c;
+	cout << CPoint::n<<endl<<endl;
+}
 
 int main(){
 	char ch;
-	system ("CLS");
 
 	do {
-		cout << "\t1) Opcion 1"<<endl;
-		cout << "\t2) Opcion 2"<<endl;
-		cout << "\t3) Opcion 3"<<endl;
-		cout << "\t4) Opcion 4"<<endl;
-		cout << "Eliga una o pulse S para salir"<<endl;
+		system("CLS");
+		menu();
 		cin >> ch;
 		switch (ch){
 		case '1':
 			cout<<"Opcion 1"<<endl;
-			cout<< "Pulse cualquier tecla para continuar."<<endl;
-			system("pause");
+			opcion1();
+			footer();
 			break;
 		case '2':
 			cout<<"Opcion 2"<<endl;
-			cout<< "Pulse cualquier tecla para continuar."<<endl;
-			system("pause");
+			footer();
 			break;
 		case '3':
 			cout<<"Opcion 3"<<endl;
-			cout<< "Pulse cualquier tecla para continuar."<<endl;
-			system("pause");
+			footer();
 			break;
 		case '4':
 			cout<<"Opcion 4"<<endl;
-			cout<< "Pulse cualquier tecla para continuar."<<endl;
-			system("pause");
+			footer();
+			break;
+		case 's':
+		case 'S':
 			break;
 		default:
 			cout<<"Eliga una opcion valida"<<endl;
-			cout<< "Pulse cualquier tecla para continuar."<<endl;
-			system("pause");
-			system("CLS");
+			footer();
 		}
 
 	}while ((ch!='S') && (ch!='s'));
