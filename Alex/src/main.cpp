73cca3958c2f38732e9a98659b176784445a83c8
Alejandro Ibarra <axelbyte@hotmail.com>
2018-02-05 20:21:19 +0100

    FASE 1. Se actualiza la presentacion del menu y la pausa y  el limpiado de la consola. (Funciona con windows pero no en Unix/Linux).

--- Alex/src/main.cpp
+++ Alex/src/main.cpp
@@ -9,21 +9,47 @@
 
 using namespace std;
 
+#ifdef _WIN32
+#include <windows.h>
+void clrscreen(){
+	system("CLS");
+}
+void pausa(){
+	system ("pause");
+}
+#else
+#include <unistd.h>
+void clrscreen(){
+	//system("CLS");
+}
+void pausa(){
+	// system ("pause");
+}
+#endif
+
 void menu() {
+	cout << "**********************"<< endl;
 	cout << "\t1) Opcion 1" << endl;
 	cout << "\t2) Opcion 2" << endl;
 	cout << "\t3) Opcion 3" << endl;
 	cout << "\t4) Opcion 4" << endl;
+	cout << "**********************"<< endl << endl;
 	cout << "Eliga una o pulse S para salir" << endl;
 }
 
 void footer() {
+
 	cout << "Pulse cualquier tecla para continuar." << endl;
-	system("pause");
+	// system("pause"); //Funciona en windows no en unix/linux
+	pausa();
+
 }
 
 void header(int opt) {
-	system ("CLS");
+
+	//system ("CLS"); //Funciona en windows no en unix/linux
+	clrscreen();
+
 	switch (opt){
 	case 1:
 		cout << "************"<<endl;
@@ -67,7 +93,8 @@ int main(){
 	char ch;
 
 	do {
-		system("CLS");
+		 // system("CLS"); //Funciona en windows no en unix/linux
+		clrscreen();
 		menu();
 		cin >> ch;
 		switch (ch){
